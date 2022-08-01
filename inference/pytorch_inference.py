e8259e05c0f1672195eb1aace06c35f92b13dfce
Gantavya Bhatt <39345600+bhattg@users.noreply.github.com>
2022-08-01 14:21:01 -0700

    Update pytorch_inference.py

--- inference/pytorch_inference.py
+++ inference/pytorch_inference.py
@@ -21,14 +21,14 @@ BATCH_SIZE = 256
 IMG_SIZE = 256
 CENTER_CROP_SIZE = 224
 NESTING_LIST=[2**i for i in range(3, 12)]
-ROOT="../" # path to validation datasets
+ROOT="../../IMAGENET/" # path to validation datasets
 
 parser=ArgumentParser()
 
 # model args
 parser.add_argument('--efficient', action='store_true', help='Efficient Flag')
 parser.add_argument('--mrl', action='store_true', help='To use MRL')
-parser.add_argument('--rep_size', default=None, help='Rep. size for fixed feature model')
+parser.add_argument('--rep_size', type=int, default=None, help='Rep. size for fixed feature model')
 parser.add_argument('--path', type=str, required=True, help='Path to .pt model checkpoint')
 parser.add_argument('--old_ckpt', action='store_true', help='To use our trained checkpoints')
 parser.add_argument('--workers', default=12, help='num workers for dataloader', type=int)
@@ -41,7 +41,7 @@ parser.add_argument('--save_softmax', action='store_true', help='To save softmax
 parser.add_argument('--save_gt', action='store_true', help='To save ground truth for model analysis')
 parser.add_argument('--save_predictions', action='store_true', help='To save predicted labels for model analysis')
 # retrieval args
-parser.add_argument('--retrieval', default=0, help='flag for image retrieval array dumps')
+parser.add_argument('--retrieval', action='store_true', help='flag for image retrieval array dumps')
 parser.add_argument('--random_sample_dim', default=4202000, help='number of random samples to slice from retrieval database', type=int)
 parser.add_argument('--retrieval_array_path', default='', help='path to save database and query arrays for retrieval', type=str)
 
@@ -73,7 +73,7 @@ test_transform = transforms.Compose([
 				normalize])
 
 # Model Eval
-if args.retrieval == 0:
+if not args.retrieval:
 	if args.dataset == 'V2':
 		print("Loading Robustness Dataset")
 		dataset = ImageNetV2Dataset("matched-frequency", transform=test_transform)
@@ -130,15 +130,14 @@ if args.retrieval == 0:
 
 
 # Image Retrieval Inference
-elif args.retrieval == 1:
-	if args.dataset_name == '1K':
-		train_path = 'path_to_imagenet1k_train/'
-		train_dataset = datasets.ImageFolder(train_path, transform=test_transform)
+else:
+	if args.dataset == '1K':
+		train_dataset = datasets.ImageFolder(ROOT+"train/", transform=test_transform)
 		test_dataset = datasets.ImageFolder(ROOT+"val/", transform=test_transform)
-	elif args.dataset_name == 'V2':
+	elif args.dataset == 'V2':
 		train_dataset = None  # V2 has only a test set
 		test_dataset = ImageNetV2Dataset("matched-frequency", transform=test_transform)
-	elif args.dataset_name == '4K':
+	elif args.dataset == '4K':
 		train_path = 'path_to_imagenet4k_train/'
 		test_path = 'path_to_imagenet4k_test/'
 		train_dataset = datasets.ImageFolder(train_path, transform=test_transform)
@@ -149,9 +148,9 @@ elif args.retrieval == 1:
 	database_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=args.workers, shuffle=False)
 	queryset_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=args.workers, shuffle=False)
 
-	config = args.dataset_name + "_val_mrl" + str(args.mrl) + "_e" + str(args.efficient) + "_ff" + str(args.rep_size)
+	config = args.dataset + "_val_mrl" + str(args.mrl) + "_e" + str(args.efficient) + "_ff" + str(args.rep_size)
 	print("Retrieval Config: " + config)
-	generate_retrieval_data(model, queryset_loader, config, args.distributed, args.dataset_name, args.random_sample_dim, args.retrieval_array_path)
-	config = args.dataset_name + "_train_mrl" + str(args.mrl) + "_e" + str(args.efficient) + "_ff" + str(args.rep_size)
+	generate_retrieval_data(model, queryset_loader, config, args.distributed, args.dataset, args.random_sample_dim, args.retrieval_array_path)
+	config = args.dataset + "_train_mrl" + str(args.mrl) + "_e" + str(args.efficient) + "_ff" + str(args.rep_size)
 	print("Retrieval Config: " + config)
-	generate_retrieval_data(model, database_loader, config, args.distributed, args.dataset_name, args.random_sample_dim, args.retrieval_array_path)
+	generate_retrieval_data(model, database_loader, config, args.distributed, args.dataset, args.random_sample_dim, args.retrieval_array_path)
